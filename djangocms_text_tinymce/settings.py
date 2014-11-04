61530f3f7635e6925ca1724c08862cd52981977e
Rafal Kokrosz <Korkki@users.noreply.github.com>
2014-11-04 10:45:48 +0100

    Update settings.py
    Default config change

--- djangocms_text_tinymce/settings.py
+++ djangocms_text_tinymce/settings.py
@@ -1,7 +1,5 @@
 from django.conf import settings
 
-from tinymce.profiles import DEFAULT
-
-TINYMCE_ADMIN_CONFIG = getattr(settings, 'TINYMCE_ADMIN_CONFIG', DEFAULT.copy())
+TINYMCE_ADMIN_CONFIG = getattr(settings, 'TINYMCE_DEFAULT_CONFIG')
 
 TEXT_SAVE_IMAGE_FUNCTION = getattr(settings, 'TEXT_SAVE_IMAGE_FUNCTION', 'djangocms_text_tinymce.picture_save.create_picture_plugin')
